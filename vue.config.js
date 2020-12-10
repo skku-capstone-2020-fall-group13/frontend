939c51bbd19ec367a32c3a079c42fc773e56c5f7
Junhyun Kim <me@junbread.win>
2020-12-11 01:44:03 +0900

    카카오맵 API 키 별도 분리 후 환경변수 추가

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
+const secrets = require('./secrets.json');
+
+process.env.VUE_APP_KAKAO_APP_KEY = secrets.kakaoAppKey;
+process.env.VUE_APP_KAKAO_REST_API_KEY = secrets.kakaoRestApiKey;
+
 module.exports = {
   transpileDependencies: [],
 };
