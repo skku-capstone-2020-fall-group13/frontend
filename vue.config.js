9c206166f395cd643bca334197c47abfa2affaec
Junhyun Kim <me@junbread.win>
2020-12-13 02:00:58 +0900

    앱 제목 환경변수화

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const secrets = require('./secrets.json');
 
+process.env.VUE_APP_NAME = '주거 쾌적도 탐색기';
 process.env.VUE_APP_KAKAO_APP_KEY = secrets.kakaoAppKey;
 process.env.VUE_APP_KAKAO_REST_API_KEY = secrets.kakaoRestApiKey;
 
